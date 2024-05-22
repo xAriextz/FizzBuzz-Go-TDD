9b46060c2593c7a7dcc26a368e378d3f57545768
xAriextz <xAriextz@gmail.comn>
2024-05-22 19:21:07 +0200

    [1] - Refactoring tests
    Creating 'TestFizzBuzzReturningTheNumberItself' which tests the cases where 'FizzBuzz' should return the number itself.

--- FizzBuzz_test.go
+++ FizzBuzz_test.go
@@ -1,18 +1,22 @@
 package fizzbuzz
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestNumber1(t *testing.T) {
-	result := FizzBuzz(1)
-	if result != "1" {
-		t.Errorf("FizzBuzz(1) returned %s, expected %s", result, "1")
+func TestFizzBuzzReturningTheNumberItself(t *testing.T) {
+	parameters := []struct {
+		input    int
+		expected string
+	}{
+		{1, "1"}, {2, "2"}, {4, "4"},
 	}
-}
 
-func TestNumber2(t *testing.T) {
-	result := FizzBuzz(2)
-	if result != "2" {
-		t.Errorf("FizzBuzz(2) returned %s, expected %s", result, "2")
+	for i := range parameters {
+		result := FizzBuzz(parameters[i].input)
+		if result != parameters[i].expected {
+			t.Errorf("FizzBuzz() returned %s, expected %s", result, parameters[i].expected)
+		}
 	}
 }
 
